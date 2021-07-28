4f421de89ad01804407224f969e4ad7f40e1a48a
Rehpe <rehpe1994@gmail.com>
2021-07-28 20:50:19 +0900

    주석 추가

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -4,11 +4,13 @@ searchBtn = document.getElementById("button-addon1");
 
 
 
-<!--처음 페이지 로딩될 때에는 검색창 숨김-->
+// 처음 페이지 로딩될 때에는 검색창 숨김
  $(document).ready(function () {
     $('#searchResultBox').hide();
     });
 
+
+ // 캐로셀 예매하기 버튼을 눌렀을 때 네이버 영화 상영일정을 팝업으로 띄워줌
 function bookingPage1() {
     window.open("https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=11410674&qvt=0&query=%EB%B3%B4%EC%8A%A4%20%EB%B2%A0%EC%9D%B4%EB%B9%84%202%20%EC%83%81%EC%98%81%EC%9D%BC%EC%A0%95",'window','location=no, directories=no,resizable=no,status=no,toolbar=no,menubar=no, width=750,height=800,left=0, top=0, scrollbars=yes')
 }
