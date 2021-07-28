883cef47ce73e68ebc13520a9fe5385dd3967e1f
Rehpe <rehpe1994@gmail.com>
2021-07-28 20:50:19 +0900

    주석 추가

--- src/api/search_api.py
+++ src/api/search_api.py
@@ -5,22 +5,7 @@ import urllib.request
 
 search_page = Blueprint('search', __name__, url_prefix='/')
 
-# <!--Naver API-->
-# client_id = "dqOa471tm1HYrs0RBMzQ"
-# client_secret = "dwy90DH1Xk"
-# encText = urllib.parse.quote("보스베이비")
-# url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
-# # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
-# request = urllib.request.Request(url)
-# request.add_header("X-Naver-Client-Id",client_id)
-# request.add_header("X-Naver-Client-Secret",client_secret)
-# response = urllib.request.urlopen(request)
-# rescode = response.getcode()
-# if(rescode==200):
-#     response_body = response.read()
-#     print(response_body.decode('utf-8'))
-# else:
-#     print("Error Code:" + rescode)
+# 네이버 영화 검색 api 호출 함수
 
 def api_search(key_value):
     client_id = "dqOa471tm1HYrs0RBMzQ"
@@ -29,7 +14,7 @@ def api_search(key_value):
     url = "https://openapi.naver.com/v1/search/movie" # json 결과
     # url = "https://openapi.naver.com/v1/search/movie.xml?query=" + encText # xml 결과
     query = "?query=" + encText
-    option = "&display=50&sort=sim"
+    option = "&display=50&sort=sim" # 검색결과는 50개까지, 유사도 순으로 검색
     url_query = url + query + option
 
     request = urllib.request.Request(url_query)
@@ -42,22 +27,6 @@ def api_search(key_value):
         return response_body.decode('utf-8')
     else:
         return "Error Code:" + rescode
-
-
-
-## API 역할을 하는 부분
-# @app.route('/review', methods=['POST'])
-# def default_post():
-#     sample_receive = request.form['sample_give']
-#     print(sample_receive)
-#     return jsonify({'msg': '이 요청은 POST!'})
-#
-#
-# @app.route('/review', methods=['GET'])
-# def default_get():
-#     sample_receive = request.args.get('sample_give')
-#     print(sample_receive)
-#     return jsonify({'msg': '이 요청은 GET!'})
 
 
 @search_page.route('/search', methods=['GET'])
