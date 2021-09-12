97892ef9134af98f32f39aab41e7dce7211e0945
Rehpe <rehpe1994@gmail.com>
2021-09-12 19:30:32 +0900

    완성본

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // searchForm = document.getElementById("search-form");
 const searchBtn = document.getElementById("jsSearchButton");
 const searchInput = document.getElementById("jsSearchInput");
@@ -94,6 +95,29 @@ function searchBtnClick() {
             }
         }
     })
+=======
+// const changeButton = document.getElementById("jsButtonChangePage");
+//
+//
+// $(document).ready(function () {
+//     $("#reviews-box").html("");
+//
+// });
+//
+// function changeButtonClick() {
+//     alert("test");
+// }
+//
+//
+// function init() {
+//     changeButton.addEventListener("click", changeButtonClick);
+// }
+// init();
+
+
+function bookingPage1() {
+    window.open("https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=11410674&qvt=0&query=%EB%B3%B4%EC%8A%A4%20%EB%B2%A0%EC%9D%B4%EB%B9%84%202%20%EC%83%81%EC%98%81%EC%9D%BC%EC%A0%95",'window','location=no, directories=no,resizable=no,status=no,toolbar=no,menubar=no, width=750,height=800,left=0, top=0, scrollbars=yes')
+>>>>>>> 6875ced (main page + Nav, Footer)
 }
 
 function listBtnClick(num, search_results) {
@@ -127,6 +151,7 @@ function listBtnClick(num, search_results) {
 
 
 
+<<<<<<< HEAD
 function init() {
     searchBtn.addEventListener("click", searchBtnClick);
     searchInput.addEventListener("keypress", function (e) {
@@ -147,3 +172,30 @@ if(searchBtn) {
 }
 
 
+=======
+// AJAX 콜
+
+// function ajaxPost() {
+//     $.ajax({
+//         type: "POST",
+//         url: "/review",
+//         data: {sample_give: '샘플데이터'},
+//         success: function (response) {
+//             alert(response["msg"]);
+//             window.location.reload();
+//         }
+//     })
+// }
+//
+// function ajaxGet() {
+//     $.ajax({
+//         type: "GET",
+//         url: "http://spartacodingclub.shop/post",
+//         data: {},
+//         success: function (response) {
+//             console.log(response);
+//             alert(response["msg"]);
+//         }
+//     })
+// }
+>>>>>>> 6875ced (main page + Nav, Footer)
